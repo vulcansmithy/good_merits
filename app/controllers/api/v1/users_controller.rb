0e3d42c898c28f40c6b7bca4a5c7970289c72550
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2016-08-13 03:52:46 +0800

    Added endpoint documentation.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::UsersController < Api::V1::BaseController
   
+  # POST  /api/users
+  # POST  /api/users, {}, { "Accept" => "application/vnd.sandbox.com; version=1" }
+  # POST  /api/users?version=1
+  # POST  /api/v1/users
   def create
   end
   
+  # GET   /api/users/:id
+  # GET   /api/users/:id, {}, { "Accept" => "application/vnd.sandbox.com; version=1" }
+  # GET   /api/users/:id?version=1
+  # GET   /api/v1/users/:id
   def show
   end
   
+  # PATCH /api/users/:id
+  # PATCH /api/users/:id, {}, { "Accept" => "application/vnd.sandbox.com; version=1" }
+  # PATCH /api/users/:id?version=1
+  # PATCH /api/v1/users/:id
   def update
   end
   
